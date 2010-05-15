3c27ef45d0a5f93a903874466e1caa8d3938808a
Thomas Nemeth <thomas@cixi.(none)>
2010-05-15 14:05:12 +0200

    Added lists management.

---
+++ lists.c
@@ -0,0 +1,84 @@
+/**
+ *  \file    lists.c
+ *  \brief   Lists handling.
+ *
+ *           Project: cybersystem (Cyberspace solar system emulation software).
+ *
+ *           Lists handling functions.
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Fri, May 14 2010
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <xmem.h>
+
+#include "0config.h"
+#include "version.h"
+#include "structures.h"
+#include "lists.h"
+
+
+void list_init(list_head * list)
+{
+        list->head = NULL;
+        list->tail = NULL;
+        list->count = 0;
+}
+
+
+void list_add(list_head * list, void * data)
+{
+        list_element * element = xmalloc(sizeof(list_element));
+
+        element->prev = list->tail;
+        element->data = data;
+        element->next = NULL;
+
+        list->tail->next = element;
+        list->tail = element;
+        list->count++;
+}
+
+
+void list_del(list_head * list, void * data)
+{
+        list_element * ptr = list->head;
+        int            found = 0;
+
+        while (ptr)
+        {
+                if (ptr->data == data)
+                {
+                        found = 1;
+                        break;
+                }
+                ptr = ptr->next;
+        }
+
+        if (! found)
+        {
+                return;
+        }
+
+        if (ptr == list->head)
+        {
+                list->head = list->head->next;
+                list->head->prev = NULL;
+        }
+        else if (ptr == list->tail)
+        {
+                list->tail = list->tail->prev;
+                list->tail->next = NULL;
+        }
+        free(ptr);
+}
+
---
+++ lists.h
@@ -0,0 +1,38 @@
+/**
+ *  \file    lists.h
+ *  \brief   Lists handling.
+ *
+ *           Project: cybersystem (Cyberspace solar system emulation software).
+ *
+ *           List handling functions.
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Fri, May 14 2010
+ */
+
+
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct t_list_element {
+        void                  * data;
+        struct t_list_element * prev;
+        struct t_list_element * next;
+} list_element;
+
+
+typedef struct {
+        list_element * head;
+        list_element * tail;
+        int            count;
+} list_head;
+
+
+void list_init(list_head * list);
+void list_add(list_head * list, void * data);
+void list_del(list_head * list, void * data);
+
+#endif /* LISTS_H */
+
