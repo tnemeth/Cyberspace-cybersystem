22e434414a491d5c62e32139251132fe0dfbf9af
Thomas Nemeth <thomas@cixi.(none)>
2010-05-15 14:05:42 +0200

    Added commands parsing.

---
+++ parse.c
@@ -0,0 +1,73 @@
+/**
+ *  \file    parse.c
+ *  \brief   Commands parsing.
+ *
+ *           Project: cybersystem (Cyberspace solar system emulation software).
+ *
+ *           File description here...
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Sat, May 15 2010
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <errors.h>
+#include <sockets.h>
+#include <packets.h>
+#include <tags.h>
+
+#include "0config.h"
+#include "version.h"
+#include "structures.h"
+#include "parse.h"
+
+
+int parse_table(cmd_table * table, client * c, unsigned char * buf)
+{
+        int i;
+        int ok = -1;
+
+
+        for (i = 0 ; table[i].key ; i++)
+        {
+                if ((buf[2] & table[i].key) == table[i].key)
+                {
+                        ok = table->cmd(c, buf);
+                }
+        }
+
+        return ok;
+}
+
+
+int parse_request(client * c, cmd_table * table)
+{
+        unsigned char buffer[MAX_PACKET_SIZE] = {0};
+        int           nb_read;
+        int           ok;
+
+        nb_read = packet_read(c->socket_tcp, buffer, MAX_PACKET_SIZE);
+        if (nb_read < 0)
+        {
+                return 0;
+        }
+
+        ok = parse_table(table, c, buffer);
+
+        switch (ok)
+        {
+                case 1:  message_send(c->socket_tcp, PACKET_MSG_ACK, 1);   break;
+                case 0:  message_send(c->socket_tcp, PACKET_MSG_NACK, 1);  break;
+                case -1: message_send(c->socket_tcp, PACKET_MSG_ERROR, 1); break;
+        }
+
+        return 1;
+}
---
+++ parse.h
@@ -0,0 +1,31 @@
+/**
+ *  \file    parse.h
+ *  \brief   Commands parsing.
+ *
+ *           Project: cybersystem (Cyberspace solar system emulation software).
+ *
+ *           File description here...
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Sat, May 15 2010
+ */
+
+
+#ifndef PARSE_H
+#define PARSE_H
+
+#include "structures.h"
+
+typedef struct {
+        unsigned char key;
+        int (* cmd)(client * c, unsigned char * buffer);
+} cmd_table;
+
+
+int parse_table(cmd_table * table, client * c, unsigned char * buf);
+int parse_request(client * c, cmd_table * table);
+
+#endif /* PARSE_H */
+
