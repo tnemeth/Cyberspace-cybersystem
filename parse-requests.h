8618f00778c702e387aea727d4c9047d92298ba0
Thomas Nemeth <thomas@cixi.(none)>
2010-05-15 14:11:32 +0200

    Fixed file renaming for requests.

---
+++ parse-requests.h
@@ -0,0 +1,25 @@
+/**
+ *  \file    parse-requests.h
+ *  \brief   Requests parsing.
+ *
+ *           Project: cybersystem (Cyberspace solar system emulation software).
+ *
+ *           This file contains the exported functions used to parse
+ *           clients requests.
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Sat, May 15 2010
+ */
+
+
+#ifndef PARSE_REQUESTS_H
+#define PARSE_REQUESTS_H
+
+#include "structures.h"
+
+void parse_god_requests(client * c);
+
+#endif /* PARSE_REQUESTS_H */
+
